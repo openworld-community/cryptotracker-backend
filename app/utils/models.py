6229bf47d4d960ab31cd7b66d555c2e7202ee512
Quielan <quielanplay@gmail.com>
2023-05-28 18:45:03 +0300

    new file:   app/utils/models.py 	new file:   app/utils/ticker.py

---
+++ app/utils/models.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+
+class TransactionWaitingRequest(BaseModel):
+    uid: int
+    event_id: int
+    amount: int
+    transaction_type: str
+    ttl: int = DEFAULT_TTL
---
+++ app/utils/ticker.py
@@ -0,0 +1,25 @@
+import asyncio
+from fastapi import BackgroundTasks
+from database import Database
+
+import logging
+
+db = Database()  # Initialize database connection
+
+db.initialize_database()
+
+async def tick():
+    """
+    Asynchronously runs an infinite loop checking for expired transactions in the database and
+    removing them. 
+    The function waits for 60 seconds after each loop before running the process again.
+    """
+    while True:
+        db.update_transaction_ttl(tick_interval)
+        db.remove_expired_transactions()
+        logging.info("Ticker ticked")
+        await asyncio.sleep(1)  # Sleep for 60 seconds before the next tick
+
+def start_ticker(background_tasks: BackgroundTasks):
+    logging.info("Ticker started")
+    background_tasks.add_task(tick)
